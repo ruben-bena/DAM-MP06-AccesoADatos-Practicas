e171f5b3bb449591b47f23a53dd6e08a76599f38
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-09-24 18:26:15 +0200

    PR111  hecha

--- PR1.1/src/main/java/com/project/PR111Files.java
+++ PR1.1/src/main/java/com/project/PR111Files.java
@@ -4,42 +4,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class PR111Files {
 
     public static void main(String[] args) {
-
-        /*
-        Des dins del programa, realitza les següents tasques:
-        -Crea una carpeta anomenada myFiles dins del directori data/pr111, la qual es troba a la ruta del projecte. La ruta del directori serà System.getProperty("user.dir") + "/data/pr111/myFiles".
-        */
 
         String camiFitxer = System.getProperty("user.dir") + "/data/pr111";
         gestionarArxius(camiFitxer);
     }
 
     public static void gestionarArxius(String camiFitxer) {
-        // Crea una carpeta anomenada myFiles dins del directori data/pr111, la qual es troba a la ruta del projecte. La ruta del directori serà System.getProperty("user.dir") + "/data/pr111/myFiles".
-        try {
 
+        try {
+    
+            // Crea una carpeta anomenada myFiles dins del directori data/pr111, la qual es troba a la ruta del projecte. La ruta del directori serà System.getProperty("user.dir") + "/data/pr111/myFiles".
             String newDirectoryString = camiFitxer + "/myFiles";
-            System.out.println("newDirectoryString=" + newDirectoryString);
             Path newDirectoryPath = Paths.get(newDirectoryString);
-            System.out.println("newDirectoryPath=" + newDirectoryPath);
-            Files.createDirectory(newDirectoryPath);
-            System.out.println("Directorio creado");
+            Files.createDirectories(newDirectoryPath);
+
+            // Dins d'aquesta carpeta, crea dos arxius: file1.txt i file2.txt.
+            String newFileString = newDirectoryString + "/file1.txt";
+            Path newFilePath = Paths.get(newFileString);
+            Files.createFile(newFilePath);
+
+            newFileString = newDirectoryString + "/file2.txt";
+            newFilePath = Paths.get(newFileString);
+            Files.createFile(newFilePath);
+
+            // Renombra l'arxiu file2.txt a renamedFile.txt.
+            Path oldFilePath = newFilePath;
+            newFileString = newDirectoryString + "/renamedFile.txt";
+            newFilePath = Paths.get(newFileString);
+            Files.move(oldFilePath, newFilePath);
+
+            // Mostra un llistat dels arxius dins de la carpeta myFiles amb el missatge: “Els arxius de la carpeta són:”.
+            Stream<Path> filesInDirectory = Files.list(newDirectoryPath);
+            System.out.println("Els arxius de la carpeta són:");
+            filesInDirectory.forEach(path -> System.out.println(path.getFileName()));
+
+            // Elimina l'arxiu file1.txt.
+            newFileString = newDirectoryString + "/file1.txt";
+            newFilePath = Paths.get(newFileString);
+            Files.delete(newFilePath);
+
+            // Torna a mostrar un llistat dels arxius dins de la carpeta myFiles amb el missatge: “Els arxius de la carpeta són:”.
+            filesInDirectory = Files.list(newDirectoryPath);
+            System.out.println("Els arxius de la carpeta són:");
+            filesInDirectory.forEach(path -> System.out.println(path.getFileName()));
 
         } catch (IOException e) {
-
             e.printStackTrace();
 
         }
 
-        // Dins d'aquesta carpeta, crea dos arxius: file1.txt i file2.txt.
-        // Renombra l'arxiu file2.txt a renamedFile.txt.
-        // Mostra un llistat dels arxius dins de la carpeta myFiles amb el missatge: “Els arxius de la carpeta són:”.
-        // Elimina l'arxiu file1.txt.
-        // Torna a mostrar un llistat dels arxius dins de la carpeta myFiles amb el missatge: “Els arxius de la carpeta són:”.
+        
         
     }
 }
