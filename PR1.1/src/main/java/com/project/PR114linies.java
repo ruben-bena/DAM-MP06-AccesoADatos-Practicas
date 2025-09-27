89312e3ed4e17524592794f4a89f87dc26b037f6
ruben-bena <ruben.bellido.navarro@gmail.com>
2025-09-27 21:54:06 +0200

    PR114 hecha

--- PR1.1/src/main/java/com/project/PR114linies.java
+++ PR1.1/src/main/java/com/project/PR114linies.java
@@ -1,5 +1,13 @@
 package com.project;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PR114linies {
 
     public static void main(String[] args) {
@@ -12,5 +20,29 @@ public class PR114linies {
 
     // Mètode per generar 10 números aleatoris i escriure'ls al fitxer
     public static void generarNumerosAleatoris(String camiFitxer) {
+        try {
+            // Generar 10 números aleatorios
+            int[] randomNumbers = new int[10];
+            for (int i=0; i<randomNumbers.length; i++) {
+                int number = (int) (Math.random()*100);
+                randomNumbers[i] = number;
+                
+            }
+
+            // Guardarlos en una lista de Strings
+            List<String> randomNumbersLines = new ArrayList<String>();
+            for (int number : randomNumbers) {
+                randomNumbersLines.add(String.valueOf(number));
+
+            }
+
+            // Escribir en un archivo
+            Path filePath = Paths.get(camiFitxer);
+            Files.write(filePath, randomNumbersLines, StandardCharsets.UTF_8);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 }
