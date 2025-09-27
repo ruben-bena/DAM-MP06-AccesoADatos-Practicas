6d33b1485545a59ed977f95b2e8c299e590947e0
ruben-bena <ruben.bellido.navarro@gmail.com>
2025-09-27 21:15:44 +0200

    PR112 hecha

--- PR1.1/src/main/java/com/project/PR112cat.java
+++ PR1.1/src/main/java/com/project/PR112cat.java
@@ -1,5 +1,12 @@
 package com.project;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class PR112cat {
 
     public static void main(String[] args) {
@@ -16,5 +23,22 @@ public class PR112cat {
 
     // Funció per mostrar el contingut de l'arxiu o el missatge d'error corresponent
     public static void mostrarContingutArxiu(String rutaArxiu) {
+        Path filePath = Paths.get(rutaArxiu);
+
+        if (Files.isDirectory(filePath)) {
+            System.out.println("El path no correspon a un arxiu, sinó a una carpeta.");
+            return;
+        }
+
+        try {
+            List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        
+        } catch (IOException e) {
+            System.out.println("El fitxer no existeix o no és accessible.");
+
+        }
     }
 }
