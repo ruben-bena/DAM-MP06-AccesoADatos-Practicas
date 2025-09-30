dc0b90fc9e093112694014fabb3532bd40ee6a97
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-09-30 19:17:54 +0200

    PR120 wip

---
+++ PR1.2/src/main/java/com/project/PR120mainPersonesHashmap.java
@@ -0,0 +1,70 @@
+package com.project;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.excepcions.IOFitxerExcepcio;
+
+public class PR120mainPersonesHashmap {
+    private static String filePath = System.getProperty("user.dir") + "/data/PR120persones.dat";
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> persones = new HashMap<>();
+        persones.put("Anna", 25);
+        persones.put("Bernat", 30);
+        persones.put("Carla", 22);
+        persones.put("David", 35);
+        persones.put("Elena", 28);
+
+        try {
+            escriurePersones(persones);
+            llegirPersones();
+        } catch (IOFitxerExcepcio e) {
+            System.err.println("Error en treballar amb el fitxer: " + e.getMessage());
+        }
+    }
+
+    // Getter per a filePath
+    public static String getFilePath() {
+        return filePath;
+    }
+
+    // Setter per a filePath
+    public static void setFilePath(String newFilePath) {
+        filePath = newFilePath;
+    }
+
+    // Mètode per escriure les persones al fitxer
+    public static void escriurePersones(HashMap<String, Integer> persones) throws IOFitxerExcepcio {
+       try (FileOutputStream fos = new FileOutputStream(getFilePath());
+            ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+
+            // Escribir el HashMap
+            oos.writeObject(persones);
+            oos.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Mètode per llegir les persones des del fitxer
+    public static void llegirPersones() throws IOFitxerExcepcio {
+        try (FileInputStream fis = new FileInputStream(getFilePath());
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+            // Leer HashMap
+            HashMap<String, Integer> persones = (HashMap<String, Integer>) ois.readObject();
+
+            // Mostrar los datos
+            for (Map.Entry<String, Integer> persona : persones.entrySet()) {
+                String nombre = persona.getKey();
+                Integer edad = persona.getValue();
+                System.out.println(nombre + ": " + edad + " anys");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
